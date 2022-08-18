15fd7af350069ba62e02e653148d7670904e39ed
mmrobi23 <robinvincent23@gmail.com>
2022-08-18 21:27:50 +0530

    new1

--- src/test/java/stepDefination/SanityTest.java
+++ src/test/java/stepDefination/SanityTest.java
@@ -7,7 +7,17 @@ public class SanityTest {
 
 		public static void main(String[] args) {
 			
+<<<<<<< Updated upstream
 			System.out.println("sanity local");
+=======
+			System.out.println("sanity test");
+			System.out.println("Normal test");
+			
+			
+			
+			
+			
+>>>>>>> Stashed changes
 		}
 		
 	}
